193ded9c22056809e75a5eb112b203fe761d523a
Str0keOOOO <786619108@qq.com>
2024-08-12 17:52:52 +0800

    fix:更新适配

--- src/anime/sidebar.ts
+++ src/anime/sidebar.ts
@@ -1,8 +1,8 @@
 import anime from "animejs";
 import { scrollToTop } from "./scrollToTop";
 
-let sidebarLeft = document.querySelector('.sidebar-left')
-let sidebarRight = document.querySelector('.sidebar-right')
+let sidebarLeft = document.querySelector('.sidebar-left') as any
+let sidebarRight = document.querySelector('.sidebar-right') as any
 let list = document.querySelector('.list') as any
 let mail = document.querySelector('.mail') as any
 let mask = document.querySelector('.sidebar-mask') as any
@@ -10,10 +10,11 @@ export function sidebar() {
   list.addEventListener('click', () => {
     scrollToTop()
     document.body.style.overflowY = "hidden";
+    sidebarLeft.style.overflow = "auto"
     mask.style.display = 'block'
     anime({
       targets: sidebarLeft,
-      translateX:'30vw',
+      translateX: '30vw',
       easing: 'easeInOutCubic',
       duration: 500,
       delay: 0
@@ -29,10 +30,11 @@ export function sidebar() {
   mail.addEventListener('click', () => {
     scrollToTop()
     document.body.style.overflowY = "hidden";
+    sidebarRight.style.overflow = "auto"
     mask.style.display = 'block'
     anime({
       targets: sidebarRight,
-      translateX:'-30vw',
+      translateX: '-31.09vw',
       easing: 'easeInOutCubic',
       duration: 500,
       delay: 0
@@ -47,17 +49,19 @@ export function sidebar() {
   })
   mask.addEventListener('click', () => {
     document.body.style.overflowY = "scroll";
+    sidebarLeft.style.overflow = "hidden"
+    sidebarRight.style.overflow = "hidden"
     mask.style.display = 'none'
     anime({
       targets: sidebarLeft,
-      translateX:'2vw',
+      translateX: '2vw',
       easing: 'easeInOutCubic',
       duration: 300,
       delay: 0
     })
     anime({
       targets: sidebarRight,
-      translateX:'-2vw',
+      translateX: '-2vw',
       easing: 'easeInOutCubic',
       duration: 300,
       delay: 0
